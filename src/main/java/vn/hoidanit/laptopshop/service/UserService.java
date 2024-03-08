df2be9f94cf6f03bfad61e7b3fa130e7a8c1aaaf
Andrew Nguyen <21520144@gm.uit.edu.vn>
2024-03-08 16:55:07 +0700

    Chapter 8

---
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -0,0 +1,37 @@
+package vn.hoidanit.laptopshop.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public String handleHello(){
+        return "Hello world with JSP";
+    }
+
+    public User handleSaveUser(User user){
+        return this.userRepository.save(user);
+    }
+
+    public List<User> getAllUsers(){
+        return this.userRepository.findAll();
+    }
+
+    public User getUserById(long id){
+        return this.userRepository.findById(id);
+    }
+
+    public void deleteUserById(long id){
+        this.userRepository.deleteById(id);
+    }
+}
