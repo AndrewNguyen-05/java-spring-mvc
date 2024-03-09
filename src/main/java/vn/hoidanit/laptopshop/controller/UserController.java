c02547868cc1e07fd0fde08bf7af72853fe3a7b2
Andrew Nguyen <21520144@gm.uit.edu.vn>
2024-03-10 00:37:24 +0700

    Chuong 9 - 10

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -12,11 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
-
 
 @Controller
 public class UserController {
